a08c2af772052077e093e190f86f38b920dd59c2
majesticrock <joshua.althueser@web.de>
2022-11-22 11:53:37 +0100

    Home

---
+++ Hubbard Mean Field/sources/Hubbard/UsingBroyden.cpp
@@ -0,0 +1,111 @@
+#define _USE_MATH_DEFINES
+
+#include "UsingBroyden.hpp"
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include "../Utility/Roots_Broyden.hpp"
+
+using Eigen::MatrixXd;
+
+namespace Hubbard {
+	void UsingBroyden::data_set::print() const {
+		std::cout << delta_cdw << "\t" << delta_sc << "\t" << delta_eta
+			<< "\t" << sqrt(delta_cdw * delta_cdw + delta_sc * delta_sc + delta_eta * delta_eta) << std::endl;
+	}
+
+	double UsingBroyden::unperturbed_energy(double k_x, double k_y) const
+	{
+		return -2 * (cos(k_x) + cos(k_y));
+	}
+
+	void UsingBroyden::fillMatrix(double k_x, double k_y)
+	{
+		hamilton.fill(0);
+
+		hamilton(0, 1) = delta_cdw;
+		hamilton(0, 2) = delta_sc;
+		hamilton(0, 3) = delta_eta;
+		hamilton(1, 2) = delta_eta;
+		hamilton(1, 3) = delta_sc;
+		hamilton(2, 3) = -delta_cdw;
+
+		Eigen::MatrixXd buffer = hamilton.transpose();
+		hamilton += buffer;
+
+		hamilton(0, 0) = unperturbed_energy(k_x, k_y);
+		hamilton(1, 1) = unperturbed_energy(k_x + M_PI, k_y + M_PI);
+		hamilton(2, 2) = -unperturbed_energy(-k_x, -k_y);
+		hamilton(3, 3) = -unperturbed_energy(-k_x + M_PI, -k_y + M_PI);
+	}
+
+	UsingBroyden::UsingBroyden(double _temperature, double _U, double _V)
+		: Model(_temperature, _U), V(_V)
+	{
+		this->delta_sc = 0.8;
+		this->delta_cdw = 0.8;
+		this->delta_eta = 0.01;
+
+		this->hamilton = MatrixXd::Zero(4, 4);
+	}
+
+	UsingBroyden::data_set UsingBroyden::compute(const bool print/*=false*/)
+	{
+		MatrixXd rho = MatrixXd::Zero(4, 4);
+		Eigen::SelfAdjointEigenSolver<MatrixXd> solver;
+
+		auto lambda_func = [&](const Eigen::VectorXd& x, Eigen::VectorXd& F) {
+			delta_cdw = x(0);
+			delta_sc = x(1);
+			delta_eta = x(2);
+			F = Eigen::Vector3d::Zero();
+			for (int k = -Constants::K_DISCRETIZATION; k < Constants::K_DISCRETIZATION; k++)
+			{
+				for (int l = -Constants::K_DISCRETIZATION; l < Constants::K_DISCRETIZATION; l++)
+				{
+					fillMatrix((k * M_PI) / Constants::K_DISCRETIZATION, (l * M_PI) / Constants::K_DISCRETIZATION);
+					solver.compute(hamilton);
+					rho.fill(0);
+
+					for (int i = 0; i < 4; i++)
+					{
+						rho(i, i) = fermi_dirac(solver.eigenvalues()(i));
+					}
+					rho = solver.eigenvectors() * rho * (solver.eigenvectors().transpose());
+
+					F(0) += rho(0, 1);
+					F(1) += rho(0, 2);
+					F(2) += rho(0, 3);
+				}
+			}
+			setParameters(F);
+			F -= x;
+		};
+		std::function<void(const Eigen::VectorXd&, Eigen::VectorXd&)> func = lambda_func;
+		Eigen::VectorXd x0 = Eigen::Vector3d(delta_cdw, delta_sc, delta_eta);
+		Eigen::VectorXd f0 = Eigen::Vector3d(delta_cdw, delta_sc, delta_eta);
+		for (size_t i = 0; i < 20; i++)
+		{
+			func(x0, f0);
+			x0(0) = delta_cdw;
+			x0(1) = delta_sc;
+			x0(2) = delta_eta;
+		}
+
+		
+		if (!Utility::Roots::Broyden::compute(func, x0)) {
+			std::cerr << "No convergence for [T, U] = [" << this->temperature << ", " << this->U << "]" << std::endl;
+			delta_cdw = 0;
+			delta_sc = 0;
+			delta_eta = 0;
+		}
+		data_set ret = { this->delta_cdw, this->delta_sc, this->delta_eta };
+
+		if (print) {
+			func(x0, f0);
+			std::cout << x0 << "\n\n" << f0 << "\n\n" << f0.norm() << std::endl;
+		}
+
+		return ret;
+	}
+}
---
+++ Hubbard Mean Field/sources/Hubbard/UsingBroyden.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include "Model.hpp"
+#include "Constants.hpp"
+
+namespace Hubbard {
+	class UsingBroyden : public Model
+	{
+	protected:
+		double delta_sc, delta_cdw, delta_eta;
+		double V;
+
+		double unperturbed_energy(double k_x, double k_y) const override;
+		virtual void fillMatrix(double k_x, double k_y) override;
+		virtual inline void setParameters(Eigen::VectorXd& F) {
+			F(0) *= (this->U - this->V) / (4 * Constants::K_DISCRETIZATION * Constants::K_DISCRETIZATION);
+			F(1) *= (this->U + this->V) / (4 * Constants::K_DISCRETIZATION * Constants::K_DISCRETIZATION);
+			F(2) *= (this->U + this->V) / (4 * Constants::K_DISCRETIZATION * Constants::K_DISCRETIZATION);
+			this->delta_cdw = F(0);
+			this->delta_sc  = F(1);
+			this->delta_eta = F(2);
+		};
+	public:
+		struct data_set {
+			double delta_cdw, delta_sc, delta_eta;
+			void print() const;
+		};
+		UsingBroyden(double _temperature, double _U, double _V);
+
+		data_set compute(const bool print = false);
+	};
+}
