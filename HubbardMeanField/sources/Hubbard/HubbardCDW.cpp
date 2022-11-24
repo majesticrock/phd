c9560d8059d4e3957c12b38335385e7fff2476c6
majesticrock <joshua.althueser@web.de>
2022-11-24 11:01:23 +0100

    test

--- HubbardMeanField/sources/Hubbard/HubbardCDW.cpp
+++ HubbardMeanField/sources/Hubbard/HubbardCDW.cpp
@@ -8,9 +8,15 @@ namespace Hubbard {
 	HubbardCDW::HubbardCDW(double _temperature, double _U, double _V)
 		: BasicHubbardModel(_temperature, _U), V(_V)
 	{
-		this->delta_cdw = 0.1;
-		this->delta_sc = 0.1;
-		this->delta_eta = 0;
+		this->delta_cdw = abs(U - V) * 0.5;
+		this->delta_sc = abs(U + V) * 0.5;
+		if (V > 0) {
+			this->delta_sc *= 0.5;
+		}
+		else {
+			this->delta_cdw *= 0.5;
+		}
+		this->delta_eta = 0.01;
 
 		this->hamilton = MatrixXd::Zero(4, 4);
 	}
