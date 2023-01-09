6daad6568eb3f4a8f59d0b52da345955d1a26258
majesticrock <joshua.althueser@web.de>
2023-01-09 17:32:56 +0100

    fix

--- HubbardMeanField/sources/Hubbard/UsingBroyden.hpp
+++ HubbardMeanField/sources/Hubbard/UsingBroyden.hpp
@@ -12,8 +12,8 @@ namespace Hubbard {
 		virtual void fillHamiltonian(double k_x, double k_y) override;
 		virtual inline void setParameters(Eigen::VectorXd& F) {
 			F(0) *= (this->U - this->V) / (8 * Constants::K_DISCRETIZATION * Constants::K_DISCRETIZATION);
-			F(1) *= (this->U + this->V) / (8 * Constants::K_DISCRETIZATION * Constants::K_DISCRETIZATION);
-			F(2) *= (this->U + this->V) / (8 * Constants::K_DISCRETIZATION * Constants::K_DISCRETIZATION);
+			F(1) *= (this->U + 0.5 * this->V) / (8 * Constants::K_DISCRETIZATION * Constants::K_DISCRETIZATION);
+			F(2) *= (this->U + 0.5 * this->V) / (8 * Constants::K_DISCRETIZATION * Constants::K_DISCRETIZATION);
 			this->delta_cdw = 0.5 * (F(0) + this->delta_cdw);
 			this->delta_sc = 0.5 * (F(1) + this->delta_sc);
 			this->delta_eta = 0.5 * (F(2) + this->delta_eta);
