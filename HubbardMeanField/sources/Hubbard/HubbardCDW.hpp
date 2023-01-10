15bc0e0f516b84bb9874dae16ada68f82a194cb6
majesticrock <joshua.althueser@web.de>
2023-01-10 16:45:30 +0100

    stuff

--- HubbardMeanField/sources/Hubbard/HubbardCDW.hpp
+++ HubbardMeanField/sources/Hubbard/HubbardCDW.hpp
@@ -9,8 +9,8 @@ namespace Hubbard {
 
 		virtual inline void setParameters(double cdw, double sc, double eta) override {
 			this->delta_cdw = ((this->U - this->V) / (4. * Constants::K_DISCRETIZATION * Constants::K_DISCRETIZATION)) * cdw;
-			this->delta_sc = ((this->U + 0.5 * this->V) / (4. * Constants::K_DISCRETIZATION * Constants::K_DISCRETIZATION)) * sc;
-			this->delta_eta = ((this->U + 0.5 * this->V) / (4. * Constants::K_DISCRETIZATION * Constants::K_DISCRETIZATION)) * eta;
+			this->delta_sc = ((this->U ) / (4. * Constants::K_DISCRETIZATION * Constants::K_DISCRETIZATION)) * sc;
+			this->delta_eta = ((this->U ) / (4. * Constants::K_DISCRETIZATION * Constants::K_DISCRETIZATION)) * eta;
 		};
 
 	public:
