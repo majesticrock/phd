1984af7a6b31106d970f4462628d89e45dd7cbfa
majesticrock <joshua.althueser@googlemail.com>
2023-01-10 15:58:19 +0100

    uni

--- HubbardMeanField/sources/Hubbard/UsingBroyden.hpp
+++ HubbardMeanField/sources/Hubbard/UsingBroyden.hpp
@@ -5,6 +5,11 @@
 namespace Hubbard {
 	class UsingBroyden : public Model
 	{
+	private:
+		Eigen::Vector4d new_sc;
+		Eigen::Vector4d new_eta;
+		Eigen::Vector4d old_sc;
+		Eigen::Vector4d old_eta;
 	protected:
 		double V;
 
@@ -12,8 +17,8 @@ namespace Hubbard {
 		virtual void fillHamiltonian(double k_x, double k_y) override;
 		virtual inline void setParameters(Eigen::VectorXd& F) {
 			F(0) *= (this->U - this->V) / (8 * Constants::K_DISCRETIZATION * Constants::K_DISCRETIZATION);
-			F(1) *= (this->U + 0.5 * this->V) / (8 * Constants::K_DISCRETIZATION * Constants::K_DISCRETIZATION);
-			F(2) *= (this->U + 0.5 * this->V) / (8 * Constants::K_DISCRETIZATION * Constants::K_DISCRETIZATION);
+			F(1) *= (this->U) / (8 * Constants::K_DISCRETIZATION * Constants::K_DISCRETIZATION); // + this->V
+			F(2) *= (this->U) / (8 * Constants::K_DISCRETIZATION * Constants::K_DISCRETIZATION); // + this->V
 			this->delta_cdw = 0.5 * (F(0) + this->delta_cdw);
 			this->delta_sc = 0.5 * (F(1) + this->delta_sc);
 			this->delta_eta = 0.5 * (F(2) + this->delta_eta);
