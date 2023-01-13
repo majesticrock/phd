1b3e4a1cc8bfe2c84fb0875bb99119f3cd43ce79
majesticrock <joshua.althueser@web.de>
2023-01-13 14:43:01 +0100

    convi

---
+++ HubbardMeanField/all_exec.sh
@@ -0,0 +1,4 @@
+mpirun -n 4 ./build/main params/params_T.config
+mpirun -n 4 ./build/main params/params_T_finite.config
+mpirun -n 4 ./build/main params/params_U.config
+mpirun -n 4 ./build/main params/params_V.config
