36e97d159bb98b72213161383f9a38498cad4ba3
majesticrock <joshua.althueser@googlemail.com>
2023-02-08 16:45:21 +0100

    WIP: Commute

--- FermionCommute/sources/FermionCommute.cpp
+++ FermionCommute/sources/FermionCommute.cpp
@@ -1,6 +1,14 @@
 #include "Term.hpp"
 
 int main(int argc, char** argv){
+    Momentum k('k');
+    Operator c_k(k, std::vector<std::string>(), false);
+    Operator c_k_dagger(k, std::vector<std::string>(), true);
 
+    Term term(1, Coefficient(), std::vector<Operator>({ 
+        c_k_dagger, c_k
+    }));
+
+    term.print();
     return 0;
 }
