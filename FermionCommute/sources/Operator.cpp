1cea9e205eb8399e93ac3966ded6748467c18892
majesticrock <joshua.althueser@web.de>
2023-02-09 17:08:58 +0100

    Line endings

--- FermionCommute/sources/Operator.cpp
+++ FermionCommute/sources/Operator.cpp
@@ -1,24 +1,111 @@
+#define append_vector(a, b) a.insert(a.end(), b.begin(), b.end())
 #include "Operator.hpp"
+
+Momentum& Momentum::operator+=(const Momentum& rhs)
+{
+	this->add_Q = (rhs.add_Q != this->add_Q);
+	append_vector(this->momentum_list, rhs.momentum_list);
+
+	std::vector<std::pair<int, char>>::iterator it = momentum_list.begin();
+	std::vector<std::pair<int, char>>::iterator jt = momentum_list.begin();
+	while (it != momentum_list.end()) {
+		jt = it + 1;
+		while (jt != momentum_list.end()) {
+			if (it->second == jt->second) {
+				it->first += jt->first;
+				jt = this->momentum_list.erase(jt);
+			}
+			else {
+				++jt;
+			}
+		}
+		if (it != momentum_list.end()) {
+			++it;
+		}
+	}
+	return *this;
+}
+
+Momentum& Momentum::operator-=(const Momentum& rhs)
+{
+	this->add_Q = (rhs.add_Q != this->add_Q);
+	const size_t old_size = this->momentum_list.size();
+	append_vector(this->momentum_list, rhs.momentum_list);
+
+	for (size_t i = old_size; i < this->momentum_list.size(); i++)
+	{
+		this->momentum_list[i].first *= -1;
+	}
+	std::vector<std::pair<int, char>>::iterator it = momentum_list.begin();
+	std::vector<std::pair<int, char>>::iterator jt = momentum_list.begin();
+	while (it != momentum_list.end()) {
+		jt = it + 1;
+		while (jt != momentum_list.end()) {
+			if (it->second == jt->second) {
+				it->first += jt->first;
+				jt = this->momentum_list.erase(jt);
+			}
+			else {
+				++jt;
+			}
+		}
+		if (it != momentum_list.end()) {
+			++it;
+		}
+	}
+	return *this;
+}
 
 void Momentum::addInPlace(const Momentum& rhs)
 {
-    this->add_Q = (rhs.add_Q != this->add_Q);
-    this->momentum_list.insert(std::end(this->momentum_list), std::begin(rhs.momentum_list), std::end(rhs.momentum_list));
-    
-    std::vector<std::pair<int, char>>::iterator it = momentum_list.begin();
-    std::vector<std::pair<int, char>>::iterator jt = momentum_list.begin();
-    while(it != momentum_list.end()){
-        jt = it + 1;
-        while(jt != momentum_list.end()){
-            if(it->second == jt->second){
-                it->first += jt->first;
-                jt = this->momentum_list.erase(jt);
-            }else{
-                ++jt;
-            }
-        }
-        if(it != momentum_list.end()){
-            ++it;
-        }
-    }
+	(*this) += rhs;
+}
+
+void Momentum::replaceOccurances(const char replaceWhat, const Momentum& replaceWith)
+{
+	for (auto it = this->momentum_list.begin(); it != this->momentum_list.end(); ++it) {
+		if (it->second == replaceWhat) {
+			auto buffer = replaceWith;
+			buffer.multiplyMomentum(it->first);
+			it = this->momentum_list.erase(it);
+
+			this->addInPlace(buffer);
+		}
+	}
+}
+
+std::ostream& operator<<(std::ostream& os, const Momentum& momentum)
+{
+	if (momentum.momentum_list.empty()) {
+		os << "0";
+		return os;
+	}
+	for (momentum_pairs::const_iterator it = momentum.momentum_list.begin(); it != momentum.momentum_list.end(); ++it)
+	{
+		if (it != momentum.momentum_list.begin() && it->first > 0) {
+			os << " +";
+		}
+		else if (it->first == -1) {
+			os << " -";
+		}
+		else if (abs(it->first) != 1) {
+			os << it->first;
+		}
+		os << it->second;
+	}
+
+	return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const Operator& op)
+{
+	os << "c_{ " << op.momentum << ", ";
+	for (const auto& index : op.indizes) {
+		os << index << " ";
+	}
+	os << "}";
+	if (op.isDaggered) {
+		os << "^\\dagger ";
+	}
+	return os;
 }
--- FermionCommute/sources/Term.hpp
+++ FermionCommute/sources/Term.hpp
@@ -4,67 +4,81 @@
 
 #include "Operator.hpp"
 
-struct Coefficient{
-    std::string name;
-    Momentum momentum;
-    // Contains all indizes, standard: first index = spin, all others arbitrary, e.g. orbitals, bands etc
-    std::vector<std::string> indizes;
-    bool isDaggered;
+struct Coefficient {
+	std::string name;
+	Momentum momentum;
+	// Contains all indizes, standard: first index = spin, all others arbitrary, e.g. orbitals, bands etc
+	std::vector<std::string> indizes;
+	bool isDaggered;
 
-    Coefficient() : name(""), momentum(), indizes(), isDaggered(false) {};
-    Coefficient(std::string _name, const Momentum& _momentum, const std::vector<std::string>& _indizes, bool _isDaggered)
-        : name(_name), momentum(_momentum), indizes(_indizes), isDaggered(_isDaggered){};
-    Coefficient(std::string _name, char _momentum, bool add_Q, const std::vector<std::string>& _indizes, bool _isDaggered)
-        : name(_name), momentum(_momentum, add_Q), indizes(_indizes), isDaggered(_isDaggered){ };
+	Coefficient() : name(""), momentum(), indizes(), isDaggered(false) {};
+	Coefficient(std::string _name, const Momentum& _momentum, const std::vector<std::string>& _indizes, bool _isDaggered)
+		: name(_name), momentum(_momentum), indizes(_indizes), isDaggered(_isDaggered) {};
+	Coefficient(std::string _name, char _momentum, bool add_Q, const std::vector<std::string>& _indizes, bool _isDaggered)
+		: name(_name), momentum(_momentum, add_Q), indizes(_indizes), isDaggered(_isDaggered) { };
 };
 
-std::ostream& operator<<(std::ostream& os, const Coefficient& coeff){
-    os << coeff.name;
-    if(!coeff.indizes.empty()){
-        os << "_{ ";
-        for(const auto& index : coeff.indizes){
-            os << index << " ";
-        }
-        os << "}";
-    }
-    if(coeff.isDaggered){
-        os << "^*";
-    }
-    if(!coeff.momentum.momentum_list.empty()){
-        os << " ( " << coeff.momentum << " )";
-    }
-
-    return os;
-};
-
-class Term{
+class Term {
 private:
-    int multiplicity;
-    Coefficient coefficient;
-    std::vector<char> sum_momenta;
-    std::vector<std::string> sum_indizes;
-    std::vector<Operator> operators;
-    // symbolises the Kronecker delta
-    std::vector<std::pair<Momentum, Momentum>> delta_momentum;
-    std::vector<std::pair<std::string, std::string>> delta_index;
+	Coefficient coefficient;
+	std::vector<char> sum_momenta;
+	std::vector<std::string> sum_indizes;
+	std::vector<Operator> operators;
+	// symbolises the Kronecker delta
+	std::vector<std::pair<Momentum, Momentum>> delta_momentum;
+	std::vector<std::pair<std::string, std::string>> delta_index;
 
 public:
-    Term(int _multiplicity, Coefficient _coefficient, const std::vector<char>& _sum_momenta, const std::vector<std::string>& _sum_indizes, const std::vector<Operator>& _operators=std::vector<Operator>())
-        : multiplicity(_multiplicity), coefficient(_coefficient), sum_momenta(_sum_momenta), sum_indizes(_sum_indizes), operators(_operators) {};
+	int multiplicity;
+	Term(int _multiplicity, Coefficient _coefficient, const std::vector<char>& _sum_momenta, const std::vector<std::string>& _sum_indizes, const std::vector<Operator>& _operators = std::vector<Operator>())
+		: coefficient(_coefficient), sum_momenta(_sum_momenta), sum_indizes(_sum_indizes), operators(_operators), multiplicity(_multiplicity) {};
 
-    Term(int _multiplicity, Coefficient _coefficient, const std::vector<char>& _sum_momenta, const std::vector<Operator>& _operators=std::vector<Operator>())
-        : multiplicity(_multiplicity), coefficient(_coefficient), sum_momenta(_sum_momenta), operators(_operators) {};
+	Term(int _multiplicity, Coefficient _coefficient, const std::vector<char>& _sum_momenta, const std::vector<Operator>& _operators = std::vector<Operator>())
+		: coefficient(_coefficient), sum_momenta(_sum_momenta), operators(_operators), multiplicity(_multiplicity) {};
 
-    Term(int _multiplicity, Coefficient _coefficient, const std::vector<std::string>& _sum_indizes, const std::vector<Operator>& _operators=std::vector<Operator>())
-        : multiplicity(_multiplicity), coefficient(_coefficient), sum_indizes(_sum_indizes), operators(_operators) {};
+	Term(int _multiplicity, Coefficient _coefficient, const std::vector<std::string>& _sum_indizes, const std::vector<Operator>& _operators = std::vector<Operator>())
+		: coefficient(_coefficient), sum_indizes(_sum_indizes), operators(_operators), multiplicity(_multiplicity) {};
 
-    Term(int _multiplicity, Coefficient _coefficient, const std::vector<Operator>& _operators=std::vector<Operator>())
-        : multiplicity(_multiplicity), coefficient(_coefficient), operators(_operators) {};
+	Term(int _multiplicity, Coefficient _coefficient, const std::vector<Operator>& _operators = std::vector<Operator>())
+		: coefficient(_coefficient), operators(_operators), multiplicity(_multiplicity) {};
 
-    inline bool isIdentity() const {
-        return this->operators.empty();
-    }
+	Term() : coefficient(), operators(), multiplicity(0) {};
 
-    void print() const;
-    friend void normalOrder(std::vector<Term>& terms);
+	inline bool isIdentity() const {
+		return this->operators.empty();
+	}
+
+	void print() const;
+	inline void flipSign() {
+		this->multiplicity *= -1;
+	}
+
+	void setDeltas();
+	void sort();
+	inline bool isEqual(const Term& other) const {
+		
+		if (this->operators.size() != other.operators.size()) return false;
+		for (size_t i = 0; i < this->operators.size(); i++)
+		{
+			if (this->operators[i] != other.operators[i]) return false;
+		}
+		return true;
+	};
+
+	friend void normalOrder(std::vector<Term>& terms);
+	friend void commutator(std::vector<Term>& reciever, const Term& left, const Term& right);
+	friend void commutator(std::vector<Term>& reciever, const std::vector<Term>& left, const std::vector<Term>& right);
+	friend std::ostream& operator<<(std::ostream& os, const Term& term);
 };
+
+inline bool operator==(const Term& lhs, const Term& rhs) {
+	return lhs.isEqual(rhs);
+};
+inline bool operator!=(const Term& lhs, const Term& rhs) {
+	return !(lhs.isEqual(rhs));
+};
+
+std::ostream& operator<<(std::ostream& os, const Coefficient& coeff);
+std::ostream& operator<<(std::ostream& os, const std::vector<Term>& terms);
+
+void cleanUp(std::vector<Term>& terms);
