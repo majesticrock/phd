2ca6d04ec24306c7fe498484d05c198e778023e8
majesticrock <joshua.althueser@googlemail.com>
2023-02-10 16:02:31 +0100

    commute WIP

--- FermionCommute/sources/FermionCommute.cpp
+++ FermionCommute/sources/FermionCommute.cpp
@@ -1,37 +1,42 @@
 #include "Term.hpp"
 
 int main(int argc, char** argv) {
-	Momentum k('k');
-	Momentum minus_k('k', -1);
-	Momentum l('l');
-	Momentum minus_l('l', -1);
-	Momentum diff(momentum_pairs({ std::make_pair(1, 'p'), std::make_pair(-1, 'k') }), false);
+	Operator c_k('k', 1, false, UP, false);
+	Operator c_minus_k('k', -1, false, DOWN, false);
 
-	Operator c_k(k, std::vector<std::string>({ UP }), false);
-	Operator c_k_dagger(k, std::vector<std::string>({ UP }), true);
-	Operator c_l(l, std::vector<std::string>({ UP }), false);
-	Operator c_l_dagger(l, std::vector<std::string>({ UP }), true);
+	Operator c_l_dagger('l', 1, false, UP, true);
+	Operator c_minus_l_dagger('l', -1, false, DOWN, true);
 
-	Operator c_minus_k(minus_k, std::vector<std::string>({ DOWN }), false);
-	Operator c_minus_k_dagger(minus_k, std::vector<std::string>({ DOWN }), true);
-	Operator c_minus_l(minus_l, std::vector<std::string>({ DOWN }), false);
-	Operator c_minus_l_dagger(minus_l, std::vector<std::string>({ DOWN }), true);
+	Term H_T(1, Coefficient("\\epsilon_0", 'q'), std::vector<char>({'q'}), std::vector<std::string>({"\\sigma"}), std::vector<Operator>({
+		Operator('q', 1, false, "\\sigma", true), Operator('q', 1, false, "\\sigma", false)
+	}));
 
-	Operator c_diff(diff, std::vector<std::string>({ UP }), false);
-	Operator c_diff_dagger(diff, std::vector<std::string>({ UP }), true);
+	Term H_U(1, Coefficient("\\frac{U}{N}"), std::vector<char>({'r', 'p', 'q'}), std::vector<Operator>({
+		Operator('r', 1, false, UP, true), Operator('p', 1, false, DOWN, true),
+		Operator(momentum_pairs({ std::make_pair(1, 'p'), std::make_pair(-1, 'q') }), DOWN, false), 
+		Operator(momentum_pairs({ std::make_pair(1, 'r'), std::make_pair(1, 'q') }), UP, false),
+	}));
+
+	std::vector<Term> H = {H_T, H_U};
 
 	Term left(1, Coefficient(), std::vector<Operator>({
 		c_minus_k, c_k
 		}));
+
 	Term right(1, Coefficient(), std::vector<Operator>({
 		c_l_dagger, c_minus_l_dagger
 		}));
 
-	std::vector<Term> terms;
-	commutator(terms, left, right);
+	std::vector<Term> commute_with_H, terms;
+	commutator(commute_with_H, H, right);
+	cleanUp(commute_with_H);
+
+	std::cout << "\\begin{align*}\n\t[ H, " << right.toStringWithoutPrefactor() << "] = " << commute_with_H << "\\end{align*}" << std::endl;
+
+	commutator(terms, left, commute_with_H);
 	cleanUp(terms);
 
-	std::cout << "\\begin{align*}\n" << "[" << left.toStringWithoutPrefactor() << ", " << right.toStringWithoutPrefactor() << "] =" 
+	std::cout << "\\begin{align*}\n\t[" << left.toStringWithoutPrefactor() << ", [ H, " << right << "] ] = " 
 		<< terms << "\\end{align*}" << std::endl;
 
 	return 0;
