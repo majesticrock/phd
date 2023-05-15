52f193c09c2bd812a2fd623ed1840ff37c1ef362
majesticrock <joshua.althueser@web.de>
2023-05-15 10:18:22 +0200

    correcter

--- cpp/HubbardMeanField/slurmy_modes.sh
+++ cpp/HubbardMeanField/slurmy_modes.sh
@@ -16,11 +16,11 @@ for NEW_VALUE in "${NEW_VALUES[@]}"; do
     if [[ $line == \#* ]]; then
       continue
     fi
-    
+
     # Split the line into token and value
     TOKEN_NAME=$(echo "$line" | awk '{print $1}')
     TOKEN_VALUE=$(echo "$line" | cut -d' ' -f2-)
-    
+
     if [[ "$TOKEN_NAME" == "$TOKEN" ]]; then
       # replace the value with the new one
       sed "s/$TOKEN_NAME $TOKEN_VALUE/$TOKEN_NAME $NEW_VALUE/" params/params_auto.config > auto_generated/params_$NEW_NAME.config
@@ -33,5 +33,5 @@ for NEW_VALUE in "${NEW_VALUES[@]}"; do
   sed -i "s|mpirun -n 1 ./build/main params/params_modes.config|mpirun -n 1 ./build/main auto_generated/params_$NEW_NAME.config|" auto_generated/$NEW_NAME.slurm
 
   # Execute the program
-  sbatch auto_generated/$NEW_VALUE.slurm
+  sbatch auto_generated/$NEW_NAME.slurm
 done
