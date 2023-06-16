6f5d10ca4ad50d57c4df2a9b659456c002a0b61b
majesticrock <joshua.althueser@web.de>
2023-06-16 15:23:00 +0200

    m neg

--- cpp/HubbardMeanField/sources/Hubbard/HubbardCDW.hpp
+++ cpp/HubbardMeanField/sources/Hubbard/HubbardCDW.hpp
@@ -22,14 +22,14 @@ namespace Hubbard {
 		std::vector<complex_prec*> param_mapper;
 		std::vector<double_prec> param_coefficients;
 	protected:
-		complex_prec delta_sc, delta_cdw, delta_afm, delta_eta;
-		complex_prec gamma_sc, xi_sc;
-		complex_prec gamma_cdw, xi_cdw;
-		complex_prec gamma_afm, xi_afm;
-		complex_prec gamma_eta, xi_eta;
+		complex_prec c_delta_sc,  c_delta_cdw, c_delta_afm, c_delta_eta;
+		complex_prec c_gamma_sc,  c_xi_sc;
+		complex_prec c_gamma_cdw, c_xi_cdw;
+		complex_prec c_gamma_afm, c_xi_afm;
+		complex_prec c_gamma_eta, c_xi_eta;
 
-		complex_prec delta_occupation_up, delta_occupation_down;
-		complex_prec delta_occupation_up_y, delta_occupation_down_y;
+		complex_prec c_delta_occupation_up, c_delta_occupation_down;
+		complex_prec c_delta_occupation_up_y, c_delta_occupation_down_y;
 
 		inline double_prec gamma_tilde(double_prec k_x, double_prec k_y) {
 			return sin(k_x) + sin(k_y);
@@ -39,12 +39,12 @@ namespace Hubbard {
 		};
 		inline virtual double_prec renormalizedEnergy_up(double_prec k_x, double_prec k_y) const override {
 			return -2. * (
-				(delta_occupation_up + 1.) * cos(k_x) + (delta_occupation_up_y + 1.) * cos(k_y)
+				(c_delta_occupation_up + 1.) * cos(k_x) + (c_delta_occupation_up_y + 1.) * cos(k_y)
 				).real();
 		};
 		inline virtual double_prec renormalizedEnergy_down(double_prec k_x, double_prec k_y) const override {
 			return -2. * (
-				(delta_occupation_down + 1.) * cos(k_x) + (delta_occupation_down_y + 1.) * cos(k_y)
+				(c_delta_occupation_down + 1.) * cos(k_x) + (c_delta_occupation_down_y + 1.) * cos(k_y)
 				).real();
 		};
 
