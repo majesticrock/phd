069f17c0167f5cd75a10a0ae0059e7c070ef831d
Joshua <joshua.althueser@gmail.com>
2023-07-20 12:54:12 +0200

    omp test

--- cpp/HubbardMeanField/sources/Hubbard/DensityOfStates/SimpleCubic.cpp
+++ cpp/HubbardMeanField/sources/Hubbard/DensityOfStates/SimpleCubic.cpp
@@ -2,6 +2,7 @@
 #include "SimpleCubic.hpp"
 #include "../Constants.hpp"
 #include "../../Utility/MidpointRule.hpp"
+#include <omp.h>
 #include <cmath>
 #include <algorithm>
 #include <numeric>
@@ -11,7 +12,7 @@
 namespace Hubbard::DensityOfStates {
 	void SimpleCubic::computeValues()
 	{
-		constexpr size_t MID_POINT_STEP_NUM = 10000;
+		const int MID_POINT_STEP_NUM = Constants::BASIS_SIZE;
 		/*
 		*  Algorithm, remember, we have t = 0.5
 		*/
@@ -19,17 +20,18 @@ namespace Hubbard::DensityOfStates {
 		values.resize(Constants::BASIS_SIZE);
 
 		auto integrand = [](const double phi, const double gamma) -> double {
-			const double pass_to_rho_2D = gamma + phi;
-			return boost::math::ellint_1(sqrt(1. - (pass_to_rho_2D * pass_to_rho_2D / 4.))) / sqrt(1 - phi * phi);
+			return boost::math::ellint_1(sqrt(1. - ((gamma + phi) * (gamma + phi) / 4.))) / sqrt(1 - phi * phi);
 		};
 
+#pragma omp parallel for num_threads(8)
 		for (int g = 0; g < Constants::BASIS_SIZE; ++g)
 		{
-			const double gamma = (0.5 + g) * step;
-			const double lower_bound = std::max(-1., -2. - gamma);
+			const double gamma = (g + 0.5) * step;
+			const double lower_bound = -1;// std::max(-1., -2. - gamma);
 			const double upper_bound = std::min(1., 2. - gamma);
-			// We extractet 1/pi^2 from the 2D DOS for computational benefit
-			values[g] = boost::math::pow<3>(M_1_PI) * Utility::NumericalSolver::Integration::midpoint_rule(integrand, lower_bound, upper_bound, MID_POINT_STEP_NUM, gamma);
+			// We extracted 1/pi^2 from the 2D DOS for computational benefit
+			values[g] = boost::math::pow<3>(M_1_PI) 
+				* Utility::NumericalSolver::Integration::midpoint_rule(integrand, lower_bound, upper_bound, MID_POINT_STEP_NUM, gamma);
 		}
 		computed = true;
 
--- cpp/HubbardMeanField/sources/Utility/MidpointRule.hpp
+++ cpp/HubbardMeanField/sources/Utility/MidpointRule.hpp
@@ -1,26 +1,29 @@
 #pragma once
 #include <utility>
 
-namespace Utility::NumericalSolver::Integration{
-    template <class UnaryFunction, class... Args>
-	auto midpoint_rule(UnaryFunction& function, double begin, const double end, const size_t num_steps, Args&&... args) {
+namespace Utility::NumericalSolver::Integration {
+	template <class UnaryFunction, class... Args>
+	auto midpoint_rule(UnaryFunction& function, double begin, const double end, unsigned long num_steps, Args&&... args) {
 		const double step = (end - begin) / num_steps;
-		auto value = function(begin + 0.5 * step, std::forward<Args>(args)...);
-		size_t iterNum = 1U;
-		do {
-			value += function(begin + (iterNum + 0.5) * step, std::forward<Args>(args)...);
-		} while (++iterNum < num_steps);
+		begin += 0.5 * step;
+		auto value = function(begin, std::forward<Args>(args)...);
+		while (--num_steps > 0)
+		{
+			begin += step;
+			value += function(begin, std::forward<Args>(args)...);
+		}
 		return step * value;
 	}
 
 	template <class UnaryFunction>
-	auto midpoint_rule(UnaryFunction& function, double begin, const double end, const size_t num_steps) {
+	auto midpoint_rule(UnaryFunction& function, double begin, const double end, unsigned long num_steps) {
 		const double step = (end - begin) / num_steps;
-		auto value = function(begin + 0.5 * step);
-		size_t iterNum = 1U;
-		do {
-			value += function(begin + (iterNum + 0.5) * step);
-		} while (++iterNum < num_steps);
+		begin += 0.5 * step;
+		auto value = function(begin);
+		while (--num_steps > 0) {
+			begin += step;
+			value += function(begin);
+		}
 		return step * value;
 	}
 }
