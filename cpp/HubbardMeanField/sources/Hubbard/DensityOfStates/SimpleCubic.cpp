e55bad230e19c80197bbeba8dff73433da96760f
Joshua <joshua.althueser@gmail.com>
2023-08-01 11:24:05 +0200

    Gauss in 3D geht

--- cpp/HubbardMeanField/sources/Hubbard/DensityOfStates/SimpleCubic.cpp
+++ cpp/HubbardMeanField/sources/Hubbard/DensityOfStates/SimpleCubic.cpp
@@ -2,11 +2,16 @@
 #include "../Constants.hpp"
 #include <omp.h>
 #include <cmath>
+#include <algorithm>
 #include <boost/math/special_functions/pow.hpp>
 #include <boost/math/quadrature/tanh_sinh.hpp>
 #include <boost/math/quadrature/gauss_kronrod.hpp>
 
 namespace Hubbard::DensityOfStates {
+	std::pair<double, double> SimpleCubic::split_limits[n_splits];
+	std::array<double, SimpleCubic::num_positions> SimpleCubic::abscissa;
+	std::array<double, SimpleCubic::num_positions> SimpleCubic::weights;
+
 	inline long double I_1(long double gamma) {
 		const long double lower_bound = std::max(-1.L, -2.L - gamma);
 		const long double upper_bound = std::min(1.L, 2.L - gamma);
@@ -37,47 +42,50 @@ namespace Hubbard::DensityOfStates {
 
 	void SimpleCubic::computeValues()
 	{
-		constexpr size_t num_positions = 30U;
 		step = 3. / Constants::BASIS_SIZE;
-		const int splits_s[] = { 3, 6, 12, 24, 48, 96, 192, 384 };
-		for (const int splits : splits_s) {
-			values.clear();
-			values.resize(splits * num_positions / 2);
-			/*
-			*  Algorithm, remember, we have t = 0.5
-			*/
-			auto getIndex = [](double k) {
-				for (size_t i = 0U; i < num_positions; ++i)
-				{
-					if (std::abs(boost::math::quadrature::gauss<double, num_positions>::abscissa()[i] - k) < 1e-12
-						|| std::abs(boost::math::quadrature::gauss<double, num_positions>::abscissa()[i] + k) < 1e-12) {
-						return i;
-					}
-				}
-				throw std::runtime_error("Did not find k within the abscissa!");
-			};
+		values.clear();
+		values.resize(n_splits * num_positions);
 
-			int offset = 0;
-			std::pair<double, double> lims{-3, -1};
-
-			auto procedure = [&](long double gamma) -> double {
-				double k = (gamma - 0.5 * (lims.first + lims.second)) * (2. / (lims.second - lims.first));
-				const int pos = offset + getIndex(k);
-				values[pos] = boost::math::pow<3>(M_1_PI) * (I_1(gamma) - I_2(gamma));
-				return values[pos];
-			};
-
-			double norm = 0;
-			for (int i = 0; i < splits; ++i)
-			{
-				lims = { -3. + i * 6. / splits, -3. + (i + 1) * 6. / splits };
-				norm += boost::math::quadrature::gauss<double, num_positions>::integrate(procedure, lims.first, lims.second);
-				offset += num_positions / 2;
-			}
-
-			//symmetrizeVector<true>(values);
-			std::cout << splits << ":    1 - NORM = " << std::scientific << 1. - norm << std::endl;
+		std::copy(boost::math::quadrature::gauss<double, num_positions>::abscissa().begin(),
+			boost::math::quadrature::gauss<double, num_positions>::abscissa().end(), abscissa.begin());
+		for (auto& a : abscissa)
+		{
+			a *= -1;
 		}
+		std::copy(boost::math::quadrature::gauss<double, num_positions>::abscissa().rbegin(),
+			boost::math::quadrature::gauss<double, num_positions>::abscissa().rend(), abscissa.begin() + num_positions / 2);
+		for (auto& a : abscissa)
+		{
+			a *= b_minus_a_halved;
+		}
+		std::cout << std::endl;
+		std::copy(boost::math::quadrature::gauss<double, num_positions>::weights().begin(),
+			boost::math::quadrature::gauss<double, num_positions>::weights().end(), weights.begin());
+		std::copy(boost::math::quadrature::gauss<double, num_positions>::weights().rbegin(),
+			boost::math::quadrature::gauss<double, num_positions>::weights().rend(), weights.begin() + num_positions / 2);
+		for (auto& w : weights)
+		{
+			w *= b_minus_a_halved;
+		}
+
+		/*
+		*  Algorithm, remember, we have t = 0.5
+		*/
+		for (int i = 0; i < n_splits; ++i)
+		{
+			split_limits[i] = {-3. + i * 6. / n_splits, -3. + (i + 1) * 6. / n_splits};
+		}
+		for (int i = 0; i < n_splits; ++i)
+		{
+			for (size_t j = 0U; j < num_positions; ++j)
+			{
+				const int pos = i * num_positions + j;
+				const double gamma = functionQuadratureOffset(i) + abscissa[j];
+
+				values[pos] = boost::math::pow<3>(LONG_1_PI) * (I_1(gamma) - I_2(gamma));
+			}
+		}
+
 		computed = true;
 	}
 }
