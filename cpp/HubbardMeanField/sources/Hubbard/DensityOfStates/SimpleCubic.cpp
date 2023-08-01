063a15bbe3d70ebcadcfbcf3061479167a63ef26
Joshua <joshua.althueser@gmail.com>
2023-08-01 14:23:16 +0200

    Back to midpoint :(

--- cpp/HubbardMeanField/sources/Hubbard/DensityOfStates/SimpleCubic.cpp
+++ cpp/HubbardMeanField/sources/Hubbard/DensityOfStates/SimpleCubic.cpp
@@ -2,18 +2,11 @@
 #include "../Constants.hpp"
 #include <omp.h>
 #include <cmath>
-#include <algorithm>
 #include <boost/math/special_functions/pow.hpp>
 #include <boost/math/quadrature/tanh_sinh.hpp>
 #include <boost/math/quadrature/gauss_kronrod.hpp>
 
 namespace Hubbard::DensityOfStates {
-	std::vector<std::pair<double, double>> SimpleCubic::split_limits;
-	std::array<double, SimpleCubic::num_positions> SimpleCubic::abscissa;
-	std::array<double, SimpleCubic::num_positions> SimpleCubic::weights;
-	int SimpleCubic::n_splits;
-	double SimpleCubic::b_minus_a_halved;
-
 	inline long double I_1(long double gamma) {
 		const long double lower_bound = std::max(-1.L, -2.L - gamma);
 		const long double upper_bound = std::min(1.L, 2.L - gamma);
@@ -44,54 +37,18 @@ namespace Hubbard::DensityOfStates {
 
 	void SimpleCubic::computeValues()
 	{
-		n_splits = 3 * Constants::K_DISCRETIZATION;
-		b_minus_a_halved = 1. / Constants::K_DISCRETIZATION;
-
 		step = 3. / Constants::BASIS_SIZE;
-		values.clear();
-		values.resize(n_splits * num_positions);
-		split_limits.resize(n_splits);
+		values.reserve(2 * Constants::BASIS_SIZE - 1);
+		values.resize(Constants::BASIS_SIZE + 1);
+		values.back() = 0.0;
 
-		std::copy(boost::math::quadrature::gauss<double, num_positions>::abscissa().begin(),
-			boost::math::quadrature::gauss<double, num_positions>::abscissa().end(), abscissa.begin());
-		for (auto& a : abscissa)
+#pragma omp parallel for
+		for (int g = 0; g < Constants::BASIS_SIZE; ++g)
 		{
-			a *= -1;
+			const double gamma = g * step;
+			values[g] = boost::math::pow<3>(LONG_1_PI) * (I_1(gamma) - I_2(gamma));
 		}
-		std::copy(boost::math::quadrature::gauss<double, num_positions>::abscissa().rbegin(),
-			boost::math::quadrature::gauss<double, num_positions>::abscissa().rend(), abscissa.begin() + num_positions / 2);
-		for (auto& a : abscissa)
-		{
-			a *= b_minus_a_halved;
-		}
-		std::cout << std::endl;
-		std::copy(boost::math::quadrature::gauss<double, num_positions>::weights().begin(),
-			boost::math::quadrature::gauss<double, num_positions>::weights().end(), weights.begin());
-		std::copy(boost::math::quadrature::gauss<double, num_positions>::weights().rbegin(),
-			boost::math::quadrature::gauss<double, num_positions>::weights().rend(), weights.begin() + num_positions / 2);
-		for (auto& w : weights)
-		{
-			w *= b_minus_a_halved;
-		}
-
-		/*
-		*  Algorithm, remember, we have t = 0.5
-		*/
-		for (int i = 0; i < n_splits; ++i)
-		{
-			split_limits[i] = { -3. + i * 6. / n_splits, -3. + (i + 1) * 6. / n_splits };
-		}
-		for (int i = 0; i < n_splits; ++i)
-		{
-			for (size_t j = 0U; j < num_positions; ++j)
-			{
-				const int pos = i * num_positions + j;
-				const double gamma = functionQuadratureOffset(i) + abscissa[j];
-
-				values[pos] = boost::math::pow<3>(LONG_1_PI) * (I_1(gamma) - I_2(gamma));
-			}
-		}
-
+		symmetrizeVector<true>(values);
 		computed = true;
 	}
 }
