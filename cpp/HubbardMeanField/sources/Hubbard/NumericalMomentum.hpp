335e3c99c79ac6f4cd91a5fe7bcc81462c2762a1
Joshua <joshua.althueser@gmail.com>
2023-09-07 12:24:59 +0200

    output nm

--- cpp/HubbardMeanField/sources/Hubbard/NumericalMomentum.hpp
+++ cpp/HubbardMeanField/sources/Hubbard/NumericalMomentum.hpp
@@ -30,4 +30,14 @@ namespace Hubbard {
 			return momenta[index];
 		};
 	};
+
+	template<unsigned int Dimension>
+	inline std::ostream& operator<<(std::ostream& os, const NumericalMomentum<Dimension>& momentum) {
+		os << momentum.momenta[0];
+		for (size_t d = 1U; d < Dimension; ++d)
+		{
+			os << " " << momentum.momenta[d];
+		}
+		return os;
+	}
 }
