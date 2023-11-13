3f969bb4c1c24be02c89710923c3d2aa93e9e9ed
Joshua <joshua.althueser@gmail.com>
2023-11-13 13:55:33 +0100

    param rename

--- cpp/HubbardMeanField/exec_modes.sh
+++ cpp/HubbardMeanField/exec_modes.sh
@@ -3,4 +3,4 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 n_mpi=1
 n_omp=$(($(nproc)/(2*$n_mpi)))
-mpirun -n $n_mpi --map-by node:PE=$n_omp --bind-to core ./build/main params/params_modes.config 2> >(while read line; do echo -e "${RED}$line${NC}"; done) 
+mpirun -n $n_mpi --map-by node:PE=$n_omp --bind-to core ./build/main params/modes.config 2> >(while read line; do echo -e "${RED}$line${NC}"; done) 
