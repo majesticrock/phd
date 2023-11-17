b8b8e02a392d01dcd4b0281687a7a1ec5089869f
Joshua <joshua.althueser@gmail.com>
2023-11-17 09:21:08 +0100

    Clean up

--- cpp/HubbardMeanField/sources/Hubbard/GlobalDefinitions.hpp
+++ cpp/HubbardMeanField/sources/Hubbard/GlobalDefinitions.hpp
@@ -15,7 +15,7 @@
 #define SC_QUAD_CUT_OFF -12
 #endif
 
-// Does not quite work yet - but the equally spaced variant is just fine 
+// Does not quite work yet - but the equally spaced variant is just fine
 // Evenly spaced might even be required
 //#define _EXACT_DOS
 
