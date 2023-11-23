5fe8038dc6962d87b9837f1c2c9ffb7a6933c0ce
Joshua <joshua.althueser@gmail.com>
2023-11-23 11:34:47 +0100

    xx

--- cpp/HubbardMeanField/exec.sh
+++ cpp/HubbardMeanField/exec.sh
@@ -1,4 +1,4 @@
 make -j
-n_mpi=1
+n_mpi=4
 n_omp=$(($(nproc)/(2*$n_mpi)))
-mpirun -n $n_mpi --map-by node:PE=$n_omp --bind-to core ./build/main params/params_$1.config
+mpirun -n $n_mpi --map-by node:PE=$n_omp --bind-to core ./build/main params/$1.config
