a95487619a92ca161198b496bc6084df8c42497e
majesticrock <joshua.althueser@googlemail.com>
2023-12-06 15:07:55 +0100

    wip

--- paper/phase_diagram.py
+++ paper/phase_diagram.py
@@ -6,35 +6,28 @@ fig, axs = plt.subplots(nrows=2, sharex=True, figsize=(6.4, 9))
 ############################################################################
 ############                  Square lattice                    ############
 ############################################################################
-micnas_d = np.loadtxt("../data/micnas_d_wave.csv").transpose()
-axs[0].plot(micnas_d[0], micnas_d[1], "k-")
-axs[0].plot(np.array([0, 2]), 0.25 * np.array([0, 2]), "k-")
-axs[0].plot(np.array([-2, 0]),        np.array([0, 0]), "k-")
-
 #Shade s-wave area
-common_x = np.linspace(-2, 0, 100)
-interpol = np.interp(common_x, micnas_d[0], micnas_d[1])
-axs[0].fill_between(common_x, np.zeros(100), interpol, color="blue", alpha=.5, label="$s$-wave")
-
-#Shade d-wave area
-axs[0].fill_between(micnas_d[0], micnas_d[1], -2 * np.ones(len(micnas_d[0])), color="red", alpha=.5, label="$d_{x^2 - y^2}$-wave")
+axs[0].fill_between([-2, 0], [0, 0], [-2, -2], color="blue", alpha=.5, label="$s$-wave")
 
 #Shade AFM area
-common_x = np.linspace(0, 2, 100)
-interpol = np.interp(common_x, micnas_d[0], micnas_d[1])
-axs[0].fill_between(common_x, .25 * common_x, interpol, color="green", alpha=.5, label="AFM")
+axs[0].fill_between([0, 2], [0, .5], [-2, -2], color="green", alpha=.5, label="AFM")
 
 #Shade CDW area
-axs[0].fill_between(np.array([-2, 0, 2]), np.array([0, 0, 0.5]), np.array([2, 2, 2]), color="orange", alpha=.5, label="CDW")
+axs[0].fill_between(np.array([-2, 0, 2]), np.array([0, 0, .5]), np.array([2, 2, 2]), color="orange", alpha=.5, label="CDW")
 
+#Shade d-wave area
+#axs[0].fill_between(micnas_d[0], micnas_d[1], -2 * np.ones(len(micnas_d[0])), color="red", alpha=.5, label="$d_{x^2 - y^2}$-wave")
+
+#Plot boundaries
+micnas_d = np.loadtxt("../data/micnas_d_wave.csv").transpose()
+axs[0].plot(micnas_d[0], micnas_d[1], "k--", label="$d_{x^2 - y^2}$-boundary")
+axs[0].plot(np.array([0, 2]), 0.25 * np.array([0, 2]),  "k-")
+axs[0].plot(np.array([-2, 0]),       np.array([0, 0]),  "k-")
+axs[0].plot(np.array([0, 0]),        np.array([0, -2]), "k-")
 
 ############################################################################
 ############                  Cubic lattice                     ############
 ############################################################################
-axs[1].plot(np.array([0, 2]),  np.array([0, 2]) / 6., "k-")
-axs[1].plot(np.array([-2, 0]), np.array([0, 0]),      "k-")
-axs[1].plot(np.array([0, 0]),  np.array([0, -2]),     "k-")
-
 #Shade s-wave area
 axs[1].fill_between([-2, 0], [0, 0], [-2, -2], color="blue", alpha=.5, label="$s$-wave")
 
@@ -44,6 +37,10 @@ axs[1].fill_between([0, 2], [0, 1./3.], [-2, -2], color="green", alpha=.5, label
 #Shade CDW area
 axs[1].fill_between(np.array([-2, 0, 2]), np.array([0, 0, 1./3.]), np.array([2, 2, 2]), color="orange", alpha=.5, label="CDW")
 
+#Plot boundaries
+axs[1].plot(np.array([0, 2]),  np.array([0, 2]) / 6., "k-")
+axs[1].plot(np.array([-2, 0]), np.array([0, 0]),      "k-")
+axs[1].plot(np.array([0, 0]),  np.array([0, -2]),     "k-")
 
 ############################################################################
 ############                 Figure settings                    ############
