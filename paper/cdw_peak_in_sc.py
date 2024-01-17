ca2631c1587cd2f327cdd5e1254da27d5565f921
majesticrock <joshua.althueser@googlemail.com>
2024-01-17 16:22:48 +0100

    uni

--- paper/cdw_peak_in_sc.py
+++ paper/cdw_peak_in_sc.py
@@ -25,78 +25,82 @@ Vs = np.array(["-0.00001", "-0.000013", "-0.000015", "-0.000017", "-0.00002", "-
                 "-0.003", "-0.004", "-0.005", "-0.006", "-0.007", "-0.008", "-0.009", 
                 "-0.01", "-0.013", "-0.015", "-0.017", "-0.02", "-0.025", 
                 "-0.03", "-0.04", "-0.05", "-0.06", "-0.07", "-0.08", "-0.09", 
-                "-0.1", "-0.13", "-0.15", "-0.2", "-0.25"
+                "-0.1", "-0.13", "-0.15", "-0.2", "-0.25", "-0.28"
                 ])
-v_data = np.log(np.array([np.abs(float(v)) for v in Vs]))
 
 folders = ["../data/modes/square/dos_3k/", "../data/modes/cube/dos_3k/"]
 element_names = ["a", "a+b", "a+ib"]
 
 name_suffix = "higgs_CDW"
-
-weights = np.zeros(len(Vs))
-peak_positions = np.zeros(len(Vs))
-peak_positions_div_delta = np.zeros(len(Vs))
-fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(12.8, 8.2), sharex=True, sharey="row", gridspec_kw=dict(hspace=0, wspace=0))
+fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(12.8, 8.2), sharex="col", sharey="row", gridspec_kw=dict(hspace=0, wspace=0))
 
 plotters = np.empty((2,2), dtype=ps.CURVEFAMILY)
 
 
 for i in range(2):
     counter = 0
-    plotters[0][i] = ps.CURVEFAMILY(6, axis=axs[0][i])
+    plotters[0][i] = ps.CURVEFAMILY(3, axis=axs[0][i])
     plotters[0][i].set_individual_colors("nice")
-    plotters[0][i].set_individual_linestyles(["-", "", "", ""])
-    plotters[0][i].set_individual_markerstyles(["", "x", "v", "1"])
+    plotters[0][i].set_individual_linestyles(["", "", "-"])
+    plotters[0][i].set_individual_markerstyles(["x", "v", ""])
     
-    plotters[1][i] = ps.CURVEFAMILY(3, axis=axs[1][i])
+    plotters[1][i] = ps.CURVEFAMILY(2, axis=axs[1][i])
     plotters[1][i].set_individual_colors("nice")
-    plotters[1][i].set_individual_linestyles([""])
-    plotters[1][i].set_individual_markerstyles(["x"])
+    plotters[1][i].set_individual_linestyles(["", "-"])
+    plotters[1][i].set_individual_markerstyles(["x", ""])
+    
+    if i == 1:
+        Vs = np.append(Vs, ["-0.3", "-0.34"])
+    v_data = np.log(np.array([np.abs(float(v)) for v in Vs]))
+    weights = np.zeros(len(Vs))
+    peak_positions = np.zeros(len(Vs))
+    peak_positions_rel = np.zeros(len(Vs))
+    peak_positions_div_delta = np.zeros(len(Vs))
     
     for T, U, V in iterate_containers(Ts, Us, Vs):
         name = f"T={T}/U={U}/V={V}"
         peak_positions[counter], weights[counter] = rp.analyze_peak(f"{folders[i]}{name}", name_suffix)
         peak_positions_div_delta[counter] = peak_positions[counter] / extract_key(f"{folders[i]}{name}/resolvent_{name_suffix}.dat.gz", "Total Gap")
+        peak_positions_rel[counter] = 2 * extract_key(f"{folders[i]}{name}/resolvent_{name_suffix}.dat.gz", "Total Gap") - peak_positions[counter]
         counter += 1
 
-    plotters[1][i].plot(v_data, weights, label="Data")
+    plotters[1][i].plot(v_data, np.exp(weights), label="Data")
+    plotters[0][i].plot(v_data, (peak_positions), label=r"$\omega_0$")
+    plotters[0][i].plot(v_data, (peak_positions_rel), label=r"$\omega_- - \omega_0$")
+    #plotters[0][i].plot(v_data, (peak_positions_div_delta), label=r"$\omega_0 / \Delta_\mathrm{SC}$")
     
-    cut = 31
-    peak_positions = np.log(peak_positions)
-    peak_positions_div_delta = np.log(peak_positions_div_delta)
-    popt, pcov = curve_fit(rp.linear_function, v_data[cut:], peak_positions[cut:])
-    x_lin = np.linspace(np.min(v_data), np.max(v_data), 2000)
-    plotters[0][i].plot(x_lin, rp.linear_function(x_lin, *popt), label="Fit")
-    axs[0][i].text(0.05, 0.9, f"$c_1={popt[0]:.4f}\pm{np.sqrt(pcov[0][0]):.4f}$", transform = axs[0][i].transAxes)
-    axs[0][i].text(0.05, 0.8, f"$d_1={popt[1]:.4f}\pm{np.sqrt(pcov[1][1]):.4f}$", transform = axs[0][i].transAxes)
-
-    plotters[0][i].plot(v_data[cut:], peak_positions[cut:], label="Data Fit")
-    plotters[0][i].plot(v_data[:cut], peak_positions[:cut], label="Omitted data")
-    plotters[0][i].plot(v_data, peak_positions_div_delta, label=r"$\omega_0 / \Delta_\mathrm{SC}$")
+    def func(x, a, b, c, d):
+        return a * (np.tanh(b * (x + c)) + d)
+    
+    popt, pcov = curve_fit(func, v_data, np.exp(weights), p0=(4.5, -0.1, 6, 1))
+    print(popt)
+    lin = np.linspace(min(v_data), max(v_data))
+    plotters[1][i].plot(lin, func(lin, *popt), label="Fit")
     
     # V = 0, Delta_CDW = 0
     peak_pos_value, peak_weight = rp.analyze_peak(f"{folders[i][:-1]}_SC/T={Ts[0]}/U={Us[0]}/V=0.0", name_suffix)
-    axs[0][i].axhline(np.log(peak_pos_value), color="black", linestyle="--")#, label="Value for $V=0$")
+    axs[0][i].axhline((peak_pos_value), color="black", linestyle="--")#, label="Value for $V=0$")
     #peak_positions_div_delta[counter] = np.copy(peak_pos_value) / extract_key(f"{folders[i][:-1]}_SC/{name}/resolvent_{name_suffix}.dat.gz", "Total Gap")
-    axs[1][i].axhline(peak_weight, color="black", linestyle="--")#, label="Value for $V=0$")
+    axs[1][i].axhline(np.exp(peak_weight), color="black", linestyle="--")#, label="Value for $V=0$")
 
 axs[0][0].title.set_text("Square")
 axs[0][1].title.set_text("Simple cubic")
-axs[1][0].set_ylim(axs[1][0].get_ylim()[0], axs[1][0].get_ylim()[1] + 0.17)
-axs[1][1].set_ylim(axs[1][1].get_ylim()[0], axs[1][1].get_ylim()[1] + 0.17)
+#axs[0][0].set_ylim(axs[0][0].get_ylim()[0], axs[0][0].get_ylim()[1] + 0.17)
+#axs[0][1].set_ylim(axs[0][1].get_ylim()[0], axs[0][1].get_ylim()[1] + 0.17)
+#axs[1][0].set_ylim(axs[1][0].get_ylim()[0], axs[1][0].get_ylim()[1] + 0.17)
+#axs[1][1].set_ylim(axs[1][1].get_ylim()[0], axs[1][1].get_ylim()[1] + 0.17)
 
-axs[0][0].text(0.8, 0.92, "(a.1)", transform = axs[0][0].transAxes)
-axs[0][1].text(0.8, 0.92, "(a.2)", transform = axs[0][1].transAxes)
-axs[1][0].text(0.8, 0.92, "(b.1)", transform = axs[1][0].transAxes)
-axs[1][1].text(0.8, 0.92, "(b.2)", transform = axs[1][1].transAxes)
+axs[0][0].text(0.88, 0.88, "(a.1)", transform = axs[0][0].transAxes)
+axs[0][1].text(0.88, 0.88, "(a.2)", transform = axs[0][1].transAxes)
+axs[1][0].text(0.88, 0.88, "(b.1)", transform = axs[1][0].transAxes)
+axs[1][1].text(0.88, 0.88, "(b.2)", transform = axs[1][1].transAxes)
 
 axs[1][0].set_xlabel(r"$\ln|V / t|$")
 axs[1][1].set_xlabel(r"$\ln|V / t|$")
-axs[1][0].set_ylabel(r"$b = \ln(w_0)$")
-axs[0][0].set_ylabel(r"$\ln(\omega_0 / t)$")
-axs[1][1].legend(loc="lower right")
-axs[0][1].legend(loc="lower right", ncol=2)
+axs[1][0].set_ylabel(r"$w_0$")
+axs[0][0].set_ylabel(r"$\omega_0$")
+axs[1][1].legend(loc="lower left")
+axs[0][1].legend(loc="upper left")
 fig.tight_layout()
 
 import os
