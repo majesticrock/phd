8052264af868d8547496e85ca47be0be93481608
Joshua <joshua.althueser@gmail.com>
2024-01-18 13:50:14 +0100

    textz

--- paper/afm_cdw_peaks_overview.py
+++ paper/afm_cdw_peaks_overview.py
@@ -16,7 +16,7 @@ import lib.plot_settings as ps
 
 Ts = np.array([0.])
 Us_base = np.array([0.0001, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4])
-Us_cube = np.array([0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0])
+Us_cube = np.array([0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0, 1.05, 1.1])
 Us = np.concatenate((-Us_base[::-1], Us_base))
 Vs = np.array([1.0])
 
