7765f4acac60571efbd51b500fca0e50d39900d8
Joshua <joshua.althueser@gmail.com>
2024-01-19 14:06:05 +0100

    better to string

--- paper/afm_cdw_peaks_details.py
+++ paper/afm_cdw_peaks_details.py
@@ -17,7 +17,7 @@ import lib.plot_settings as ps
 Ts = np.array([0.])
 Us = np.array([0.0001, 0.00015, 0.0002, 0.0003, 0.0004, 0.0005, 0.0007, 0.001, 0.0015, 0.002, 0.003, 0.004, 0.005, 0.007, 
                 0.01, 0.015, 0.02, 0.03, 0.04, 0.05, 0.07, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4])
-Us_cube = np.array([0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0, 1.1])
+Us_cube = np.array([0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0, 1.05, 1.1])
 Vs = np.array([1.0])
 
 
@@ -57,8 +57,15 @@ for i in range(2):
             peak_positions_to_cont[counter] = cont_edges[0] - peak_positions[counter]
             counter += 1
         
-        plotters[0][i].plot(np.log(Us), (peak_positions_to_cont), label=f"Data - $\\mathcal{{A}}_\\mathrm{{{name_suffix}}} (\\omega)$")
-        plotters[1][i].plot(np.log(Us), np.exp(weights))
+        # V = 0, Delta_CDW = 0
+        peak_pos_value, peak_weight = rp.analyze_peak(f"{folders[i]}T={Ts[0]}/U={4.0 if i == 0 else 6.0}/V=1.0", f"higgs_{name_suffix}")
+        cont_edges = cf.continuum_edges(f"{folders[i]}T={Ts[0]}/U={4.0 if i == 0 else 6.0}/V=1.0", f"higgs_{name_suffix}", True)
+        peak_pos_value = cont_edges[0] - peak_pos_value
+        #axs[0][i].axhline((peak_pos_value), color="black", linestyle="--")
+        #axs[1][i].axhline(np.exp(peak_weight), color="black", linestyle="--")
+        
+        plotters[0][i].plot(np.log(Us), np.log(np.abs(peak_pos_value - peak_positions_to_cont)), label=f"Data - $\\mathcal{{A}}_\\mathrm{{{name_suffix}}} (\\omega)$")
+        plotters[1][i].plot(np.log(Us), ((np.exp(peak_weight) - np.exp(weights))))
 
 axs[0][0].title.set_text("Square")
 axs[0][1].title.set_text("Simple cubic")
