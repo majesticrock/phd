e7a19039e7354300dca3cecef536e5a560559c21
majesticrock <joshua.althueser@googlemail.com>
2024-01-24 16:14:14 +0100

    uni

--- paper/phase_diagram.py
+++ paper/phase_diagram.py
@@ -3,17 +3,21 @@ import matplotlib.pyplot as plt
 
 fig, axs = plt.subplots(nrows=2, sharex=True, figsize=(6.4, 9))
 
+xlims = (-2, 2)
+ylims = (-2, 2)
+alpha = .75
+
 ############################################################################
 ############                  Square lattice                    ############
 ############################################################################
 #Shade s-wave area
-axs[0].fill_between([-2, 0], [0, 0], [-2, -2], color="blue", alpha=.5, label="$s$-wave")
-
-#Shade AFM area
-axs[0].fill_between([0, 2], [0, .5], [-2, -2], color="green", alpha=.5, label="AFM")
+axs[0].fill_between([xlims[0], 0], [0, 0], [ylims[0], ylims[0]], alpha=alpha, label="$s$-wave")
 
 #Shade CDW area
-axs[0].fill_between(np.array([-2, 0, 2]), np.array([0, 0, .5]), np.array([2, 2, 2]), color="orange", alpha=.5, label="CDW")
+axs[0].fill_between(np.array([xlims[0], 0, xlims[1]]), np.array([0, 0, xlims[1] / 4]), np.array([ylims[1], ylims[1], ylims[1]]), alpha=alpha, label="CDW")
+
+#Shade AFM area
+axs[0].fill_between([0, xlims[1]], [0, xlims[1] / 4], [ylims[0], ylims[0]], alpha=alpha, label="AFM")
 
 #Shade d-wave area
 #axs[0].fill_between(micnas_d[0], micnas_d[1], -2 * np.ones(len(micnas_d[0])), color="red", alpha=.5, label="$d_{x^2 - y^2}$-wave")
@@ -21,44 +25,44 @@ axs[0].fill_between(np.array([-2, 0, 2]), np.array([0, 0, .5]), np.array([2, 2, 
 #Plot boundaries
 micnas_d = np.loadtxt("../data/micnas_d_wave.csv").transpose()
 axs[0].plot(micnas_d[0], micnas_d[1], "k--", label="$d_{x^2 - y^2}$-boundary")
-axs[0].plot(np.array([0, 2]), 0.25 * np.array([0, 2]),  "k-")
-axs[0].plot(np.array([-2, 0]),       np.array([0, 0]),  "k-")
-axs[0].plot(np.array([0, 0]),        np.array([0, -2]), "k-")
+axs[0].plot(np.array([0, xlims[1]]), np.array([0, xlims[1]]) / 4,  "k-")
+axs[0].plot(np.array([xlims[0], 0]), np.array([0, 0]),  "k-")
+axs[0].plot(np.array([0, 0]),        np.array([0, ylims[0]]), "k-")
 
 ############################################################################
 ############                  Cubic lattice                     ############
 ############################################################################
 #Shade s-wave area
-axs[1].fill_between([-2, 0], [0, 0], [-2, -2], color="blue", alpha=.5, label="$s$-wave")
-
-#Shade AFM area
-axs[1].fill_between([0, 2], [0, 1./3.], [-2, -2], color="green", alpha=.5, label="AFM")
+axs[1].fill_between([xlims[0], 0], [0, 0], [ylims[0], ylims[0]], alpha=alpha, label="$s$-wave")
 
 #Shade CDW area
-axs[1].fill_between(np.array([-2, 0, 2]), np.array([0, 0, 1./3.]), np.array([2, 2, 2]), color="orange", alpha=.5, label="CDW")
+axs[1].fill_between(np.array([xlims[0], 0, xlims[1]]), np.array([0, 0, xlims[1] / 6]), np.array([ylims[1], ylims[1], ylims[1]]), alpha=alpha, label="CDW")
+
+#Shade AFM area
+axs[1].fill_between([0, xlims[1]], [0, xlims[1] / 6], [ylims[0], ylims[0]], alpha=alpha, label="AFM")
 
 #Plot boundaries
-axs[1].plot(np.array([0, 2]),  np.array([0, 2]) / 6., "k-")
-axs[1].plot(np.array([-2, 0]), np.array([0, 0]),      "k-")
-axs[1].plot(np.array([0, 0]),  np.array([0, -2]),     "k-")
+axs[1].plot(np.array([0, xlims[1]]), np.array([0, xlims[1]]) / 6,  "k-")
+axs[1].plot(np.array([xlims[0], 0]), np.array([0, 0]),  "k-")
+axs[1].plot(np.array([0, 0]),        np.array([0, ylims[0]]), "k-")
 
 ############################################################################
 ############                 Figure settings                    ############
 ############################################################################
-axs[0].set_xlim(-2, 2)
-axs[0].set_ylim(-2, 2)
-axs[1].set_ylim(-2, 2)
+axs[0].set_xlim(xlims[0], xlims[1])
+axs[0].set_ylim(ylims[0], ylims[1])
+axs[1].set_ylim(ylims[0], ylims[1])
 
-axs[0].text(1.7, 1.7, "(a)")
-axs[1].text(1.7, 1.7, "(b)")
+axs[0].text(xlims[1] - np.abs(xlims[1] - xlims[0]) * 0.15, ylims[1] - np.abs(ylims[1] - ylims[0]) * 0.15, "(a)")
+axs[1].text(xlims[1] - np.abs(xlims[1] - xlims[0]) * 0.15, ylims[1] - np.abs(ylims[1] - ylims[0]) * 0.15, "(b)")
 
 axs[1].set_xlabel("$U / t$")
 axs[0].set_ylabel("$V / t$")
 axs[1].set_ylabel("$V / t$")
 
-axs[1].set_xticks([-2, -1, 0, 1, 2])
-axs[0].set_yticks([-2, -1, 0, 1, 2])
-axs[1].set_yticks([-2, -1, 0, 1, 2])
+#axs[1].set_xticks([-2, -1, 0, 1, 2])
+#axs[0].set_yticks([-2, -1, 0, 1, 2])
+#axs[1].set_yticks([-2, -1, 0, 1, 2])
 
 axs[0].legend(loc="upper left")
 plt.tight_layout()
---
+++ paper/phase_limits.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig, axs = plt.subplots(nrows=2, sharex=True, figsize=(6.4, 9))
+
+xlims = (-5, 0)
+ylims = (-0.5, 0)
+alpha = .75
+
+############################################################################
+############                  Square lattice                    ############
+############################################################################
+import gzip
+with gzip.open(f"../data/phases/square/T0/unkown_boundary.dat.gz", 'rt') as fp:
+    u_boundary = np.loadtxt(fp)
+
+axs[0].fill_between(u_boundary[0], u_boundary[1], np.full(len(u_boundary[0]), ylims[1]), label="$s$-wave", alpha=alpha)
+axs[0].fill_between(u_boundary[0], u_boundary[1], np.full(len(u_boundary[0]), ylims[0]), label="New phases", alpha=alpha)
+axs[0].fill_between([u_boundary[0][-1], xlims[1]], [ylims[1], ylims[1]], [ylims[0], ylims[0]], label=r"$\Delta$ too small", alpha=alpha)
+axs[0].plot(u_boundary[0], u_boundary[1], "k-")
+axs[0].axvline(u_boundary[0][-1], color="k", linestyle="--")
+
+############################################################################
+############                  Cubic lattice                     ############
+############################################################################
+with gzip.open(f"../data/phases/cube/T0/unkown_boundary.dat.gz", 'rt') as fp:
+    u_boundary = np.loadtxt(fp)
+
+axs[1].fill_between(u_boundary[0], u_boundary[1], np.full(len(u_boundary[0]), ylims[1]), label="$s$-wave", alpha=alpha)
+axs[1].fill_between(u_boundary[0], u_boundary[1], np.full(len(u_boundary[0]), ylims[0]), label="New phases", alpha=alpha)
+axs[1].fill_between([u_boundary[0][-1], xlims[1]], [ylims[1], ylims[1]], [ylims[0], ylims[0]], label=r"$\Delta$ too small", alpha=alpha)
+axs[1].plot(u_boundary[0], u_boundary[1], "k-")
+axs[1].axvline(u_boundary[0][-1], color="k", linestyle="--")
+
+############################################################################
+############                 Figure settings                    ############
+############################################################################
+axs[0].set_xlim(xlims[0], xlims[1])
+axs[0].set_ylim(ylims[0], ylims[1])
+axs[1].set_ylim(ylims[0], ylims[1])
+
+axs[0].text(xlims[1] - np.abs(xlims[1] - xlims[0]) * 0.15, ylims[1] - np.abs(ylims[1] - ylims[0]) * 0.15, "(a)")
+axs[1].text(xlims[1] - np.abs(xlims[1] - xlims[0]) * 0.15, ylims[1] - np.abs(ylims[1] - ylims[0]) * 0.15, "(b)")
+
+axs[1].set_xlabel("$U / t$")
+axs[0].set_ylabel("$V / t$")
+axs[1].set_ylabel("$V / t$")
+
+#axs[1].set_xticks([-2, -1, 0, 1, 2])
+#axs[0].set_yticks([-2, -1, 0, 1, 2])
+#axs[1].set_yticks([-2, -1, 0, 1, 2])
+
+axs[0].legend(loc="upper left")
+plt.tight_layout()
+import os
+plt.savefig(f"plots/{os.path.basename(__file__).split('.')[0]}.pdf")
+#plt.show()
