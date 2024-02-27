4f053ce4f171d9ed484db1f8ad7725d1d70c5498
majesticrock <joshua.althueser@googlemail.com>
2024-02-27 14:20:48 +0100

    renaming slurm

--- paper/afm_cdw_peaks_details.py
+++ paper/afm_cdw_peaks_details.py
@@ -24,7 +24,6 @@ Us_cube   = np.array([ 0.0001, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45,
 Us = np.array([Us_square, Us_cube], dtype=object)
 Vs = np.array([1.0])
 
-
 folders = ["../data/modes/square/dos_3000/", "../data/modes/cube/dos_3000/"]
 element_names = ["a", "a+b", "a+ib"]
 
