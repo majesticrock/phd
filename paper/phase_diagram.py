bca83ececaed22c0e156e9649653d874ef46640a
majesticrock <joshua.althueser@googlemail.com>
2024-02-28 15:59:30 +0100

    uni

--- paper/phase_diagram.py
+++ paper/phase_diagram.py
@@ -2,16 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 fig, axs = plt.subplots(nrows=2, sharex=True, figsize=(6.4, 9))
+folder = "T0"
 
-xlims = (-2, 2)
+xlims = (-4, 4)
 ylims = (-2, 2)
 alpha = .6
 
 ############################################################################
 ############                  Square lattice                    ############
 ############################################################################
+import gzip
+with gzip.open(f"../data/phases/square/{folder}/unkown_boundary.dat.gz", 'rt') as fp:
+    u_boundary = np.loadtxt(fp)
+
 #Shade s-wave area
-axs[0].fill_between([xlims[0], 0], [0, 0], [ylims[0], ylims[0]], alpha=alpha, label="$s$-wave")
+axs[0].fill_between(u_boundary[0], u_boundary[1], np.full(len(u_boundary[0]), 0), label="$s$-wave", alpha=alpha)
 
 #Shade CDW area
 axs[0].fill_between(np.array([xlims[0], 0, xlims[1]]), np.array([0, 0, xlims[1] / 4]), np.array([ylims[1], ylims[1], ylims[1]]), alpha=alpha, label="CDW")
@@ -19,8 +24,11 @@ axs[0].fill_between(np.array([xlims[0], 0, xlims[1]]), np.array([0, 0, xlims[1] 
 #Shade AFM area
 axs[0].fill_between([0, xlims[1]], [0, xlims[1] / 4], [ylims[0], ylims[0]], alpha=alpha, label="AFM")
 
-#Shade d-wave area
-#axs[0].fill_between(micnas_d[0], micnas_d[1], -2 * np.ones(len(micnas_d[0])), color="red", alpha=.5, label="$d_{x^2 - y^2}$-wave")
+# unknown regions
+axs[0].fill_between(u_boundary[0], u_boundary[1], np.full(len(u_boundary[0]), ylims[0]), label="Unknown phases", alpha=alpha)
+axs[0].fill_between([u_boundary[0][-1], 0], [0, 0], [ylims[0], ylims[0]], label=r"$\Delta$ too small", alpha=alpha)
+axs[0].plot(u_boundary[0], u_boundary[1], "k-")
+axs[0].axvline(u_boundary[0][-1], ymin=0, ymax=0.5, color="k", linestyle=":")
 
 #Plot boundaries
 micnas_d = np.loadtxt("../data/micnas_d_wave.csv").transpose()
@@ -32,14 +40,23 @@ axs[0].plot(np.array([0, 0]),        np.array([0, ylims[0]]), "k-")
 ############################################################################
 ############                  Cubic lattice                     ############
 ############################################################################
+with gzip.open(f"../data/phases/cube/{folder}/unkown_boundary.dat.gz", 'rt') as fp:
+    u_boundary = np.loadtxt(fp)
+
 #Shade s-wave area
-axs[1].fill_between([xlims[0], 0], [0, 0], [ylims[0], ylims[0]], alpha=alpha, label="$s$-wave")
+axs[1].fill_between(u_boundary[0], u_boundary[1], np.full(len(u_boundary[0]), 0), label="$s$-wave", alpha=alpha)
 
 #Shade CDW area
 axs[1].fill_between(np.array([xlims[0], 0, xlims[1]]), np.array([0, 0, xlims[1] / 6]), np.array([ylims[1], ylims[1], ylims[1]]), alpha=alpha, label="CDW")
 
 #Shade AFM area
 axs[1].fill_between([0, xlims[1]], [0, xlims[1] / 6], [ylims[0], ylims[0]], alpha=alpha, label="AFM")
+
+# unknown regions
+axs[1].fill_between(u_boundary[0], u_boundary[1], np.full(len(u_boundary[0]), ylims[0]), label="Unknown phases", alpha=alpha)
+axs[1].fill_between([u_boundary[0][-1], 0], [0, 0], [ylims[0], ylims[0]], label=r"$\Delta$ too small", alpha=alpha)
+axs[1].plot(u_boundary[0], u_boundary[1], "k-")
+axs[1].axvline(u_boundary[0][-1], ymin=0, ymax=0.5, color="k", linestyle=":")
 
 #Plot boundaries
 axs[1].plot(np.array([0, xlims[1]]), np.array([0, xlims[1]]) / 6,  "k-")
@@ -53,18 +70,14 @@ axs[0].set_xlim(xlims[0], xlims[1])
 axs[0].set_ylim(ylims[0], ylims[1])
 axs[1].set_ylim(ylims[0], ylims[1])
 
-axs[0].text(xlims[1] - np.abs(xlims[1] - xlims[0]) * 0.15, ylims[1] - np.abs(ylims[1] - ylims[0]) * 0.15, "(a)")
-axs[1].text(xlims[1] - np.abs(xlims[1] - xlims[0]) * 0.15, ylims[1] - np.abs(ylims[1] - ylims[0]) * 0.15, "(b)")
+axs[0].text(-3.85, 1.65, "(a)")
+axs[1].text(-3.85, 1.65, "(b)")
 
 axs[1].set_xlabel("$U [t]$")
 axs[0].set_ylabel("$V [t]$")
 axs[1].set_ylabel("$V [t]$")
 
-#axs[1].set_xticks([-2, -1, 0, 1, 2])
-#axs[0].set_yticks([-2, -1, 0, 1, 2])
-#axs[1].set_yticks([-2, -1, 0, 1, 2])
-
-axs[0].legend(loc="upper left")
+axs[0].legend(loc="upper right", ncol=2)
 plt.tight_layout()
 import os
 plt.savefig(f"plots/{os.path.basename(__file__).split('.')[0]}.pdf")
