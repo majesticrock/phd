2f0526a64dc63cd02347eb8202ee723f6e58ff7f
majesticrock <joshua.althueser@googlemail.com>
2024-02-28 09:26:37 +0100

    uboundary opt

--- cpp/HubbardMeanField/sources/Hubbard/Helper/UtilizeBlockStructure.cpp
+++
@@ -1,8 +0,0 @@
-#include "UtilizeBlockStructure.hpp"
-
-namespace Hubbard::Helper {
-	std::vector<ResolventReturnData> UtilizeBlockStructure::computeCollectiveModes(std::vector<std::vector<global_floating_type>>& reciever)
-	{
-		return std::vector<ResolventReturnData>();
-	}
-}
