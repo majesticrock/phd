e21ae17e945b4c41ade722bb295a5523a611bf8d
Joshua <joshua.althueser@gmail.com>
2024-03-01 12:03:52 +0100

    okok

--- cpp/HubbardMeanField/sources/TestHandler.cpp
+++ cpp/HubbardMeanField/sources/TestHandler.cpp
@@ -25,6 +25,7 @@ std::ostream& operator<<(std::ostream& os, const std::vector<double>& data) {
 }
 
 #include <Eigen/Sparse>
+#include "Utility/PivotToBlockStructure.hpp"
 using namespace Eigen;
 
 void TestHandler::execute(Utility::InputFileReader& input) const
