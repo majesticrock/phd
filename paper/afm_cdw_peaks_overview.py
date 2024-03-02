2bf9da209311dfb75d86255b84536052f896b529
Joshua <joshua.althueser@gmail.com>
2024-03-02 10:21:39 +0100

    ty vole

--- paper/afm_cdw_peaks_overview.py
+++ paper/afm_cdw_peaks_overview.py
@@ -21,12 +21,13 @@ Us_cube = np.array([0.0001, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09
 #    print(f"0 {4.8 + U} 0.8")
 
 Ts = np.array([0.])
-Us_square = np.array([0.0001, 0.025, 0.05, 0.075, 0.1, 0.125, 0.15, 0.175, 0.2, 0.225, 0.25, 0.275, 0.3, 0.325, 0.35, 0.375, 0.4, 0.41, 0.42, 0.423])   # there is a peak at 0.424, but our numerical tools start breaking 
-Us_cube = np.array([0.0001, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0, 1.05, 1.105])
+#Us_square = np.array([0.0001, 0.025, 0.05, 0.075, 0.1, 0.125, 0.15, 0.175, 0.2, 0.225, 0.25, 0.275, 0.3, 0.325, 0.35, 0.375, 0.4, 0.41, 0.42, 0.423])   # there is a peak at 0.424, but our numerical tools start breaking 
+#Us_cube = np.array([0.0001, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0, 1.05, 1.105])
 Us = np.array([np.concatenate((-Us_square[::-1], Us_square)), np.concatenate((-Us_cube[::-1], Us_cube))], dtype=object)
+u_data = 4.8 + Us[0]
 Vs = np.array([1.0])
 
-folders = ["../data/modes/square/dos_3000/", "../data/modes/cube/dos_3000/"]
+folders = ["../data/modes/square/dos_6000/", "../data/modes/cube/dos_6000/"]
 element_names = ["a", "a+b", "a+ib"]
 
 name_suffices = ["AFM", "CDW"]
@@ -40,10 +41,6 @@ for i in range(2):
         plotters[j][i].set_individual_colors("nice")
         plotters[j][i].set_individual_linestyles(["-", "-"])
         plotters[j][i].set_individual_markerstyles(["v", "^"])
-    if i == 0:
-        u_data = 4.0 + Us[0]
-    else:
-        u_data = 6.0 + Us[1]
 
     weights = np.zeros(len(u_data))
     peak_positions = np.zeros(len(u_data))
@@ -51,7 +48,8 @@ for i in range(2):
     
     for name_suffix in name_suffices:
         counter = 0
-        for T, U, V in iterate_containers(Ts, u_data, Vs):
+        for T, U, V_a in iterate_containers(Ts, u_data, Vs):
+            V = 1.2 - i * 0.4
             name = f"T={T}/U={round(U, 4)}/V={V}"
             cont_edges = cf.continuum_edges(f"{folders[i]}{name}", f"higgs_{name_suffix}", True)
             lower = 0 if float(V) < 1 else float(V)
