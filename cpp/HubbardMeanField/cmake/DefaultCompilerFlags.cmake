552ffbea94e26e79a3099b0ebd88ef8dda741ba3
majesticrock <joshua.althueser@googlemail.com>
2024-06-28 14:55:02 +0200

    hmf

--- cpp/HubbardMeanField/cmake/DefaultCompilerFlags.cmake
+++ cpp/HubbardMeanField/cmake/DefaultCompilerFlags.cmake
@@ -1,11 +1,17 @@
 # DefaultCompilerFlags.cmake
 
-if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
-    if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 12.1)
-        message(FATAL_ERROR "GCC version ${CMAKE_CXX_COMPILER_VERSION} is not supported. GCC 12.1 or newer is required for C++20 support.")
+function(SET_COMPILER_FLAGS TARGET)
+    if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+        if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 12.1)
+            message(FATAL_ERROR "GCC version ${CMAKE_CXX_COMPILER_VERSION} is not supported. GCC 12.1 or newer is required for C++20 support.")
+        endif()
+
+        target_compile_options(${TARGET} PRIVATE -Wall -Wno-sign-compare -fopenmp -march=native -O3)
+
+        if(NOT USE_MPI)
+            target_compile_definitions(${TARGET} PRIVATE _NO_MPI)
+        endif()
+    else()
+        message(FATAL_ERROR "Unsupported compiler ${CMAKE_CXX_COMPILER_ID}. Only GCC is supported.")
     endif()
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wno-sign-compare -fopenmp -march=native -O3")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-sign-compare -fopenmp -march=native -O3")
-else()
-    message(FATAL_ERROR "Unsupported compiler ${CMAKE_CXX_COMPILER_ID}. Only GCC is supported.")
-endif()
+endfunction()
