70b0cebf36109906a449d242cd9efe6f6f6422e7
Joshua <joshua.althueser@gmail.com>
2024-10-04 13:07:27 +0200

    hubbard stuff

--- cpp/HubbardMeanField/sources/Handler/ModeDispersionHandler.cpp
+++ cpp/HubbardMeanField/sources/Handler/ModeDispersionHandler.cpp
@@ -8,7 +8,7 @@ const std::string BASE_FOLDER = "../../data/hubbard/";
 
 void ModeDispersionHandler::execute(Utility::InputFileReader& input) const
 {
-/*	using std::to_string;
+	using std::to_string;
 
 	std::vector<double> model_params = input.getDoubleList("model_parameters");
 	Hubbard::Models::ModelParameters modelParameters(model_params[0], model_params[1], model_params[2],
@@ -18,18 +18,27 @@ void ModeDispersionHandler::execute(Utility::InputFileReader& input) const
 	Hubbard::Helper::SquareXP modeHelper(input, modelParameters);
 
 	const int TOTAL_EVAL_POINTS = 3 * Hubbard::Constants::K_DISCRETIZATION;
-	for(int i = 0; i < TOTAL_EVAL_POINTS; ++i)
+	for(int i = 0; i < Hubbard::Constants::K_DISCRETIZATION; ++i)
 	{
 		modeHelper.computeCollectiveModes();
-		if(i == TOTAL_EVAL_POINTS - 1){
-			resolvents = modeHelper.computeCollectiveModes();
-		}
+		modeHelper.mode_momentum.x() += 1;
+	}
+	for (int i = 0; i < Hubbard::Constants::K_DISCRETIZATION; ++i)
+	{
+		modeHelper.computeCollectiveModes();
+		modeHelper.mode_momentum.y() += 1;
+	}
+	for (int i = 0; i < Hubbard::Constants::K_DISCRETIZATION; ++i)
+	{
+		modeHelper.computeCollectiveModes();
+		modeHelper.mode_momentum.x() -= 1;
+		modeHelper.mode_momentum.y() -= 1;
 	}
 
 	const std::string output_folder{ getOutputFolder(input) + modelParameters.getFolderName() };
 	std::cout << "Saving data to folder " << BASE_FOLDER + output_folder << std::endl;
 	std::filesystem::create_directories(BASE_FOLDER + output_folder);
-	const std::vector<std::string> comments = getFileComments(input, modeHelper.get());
+	const std::vector<std::string> comments = getFileComments(input, &modeHelper);
 	if (!resolvents.empty()) {
 		nlohmann::json jResolvents = {
 			{ "resolvents", resolvents },
@@ -37,15 +46,15 @@ void ModeDispersionHandler::execute(Utility::InputFileReader& input) const
 			{ "Used DOS", input.getBool("use_DOS") },
 			{ "Discretization", input.getInt("k_discretization") },
 			{ "Lattice type", input.getString("lattice_type") },
-			{ "Total Gap", modeHelper->getModel().getTotalGapValue() },
-			{ "Continuum Boundaries", modeHelper->getModel().continuum_boundaries() },
-			{ "T", modeHelper->getModel().temperature }, 
-			{ "U", modeHelper->getModel().U }, 
-			{ "V", modeHelper->getModel().V }
+			{ "Total Gap", modeHelper.getModel().getTotalGapValue() },
+			{ "Continuum Boundaries", modeHelper.getModel().continuum_boundaries() },
+			{ "T", modeHelper.getModel().temperature }, 
+			{ "U", modeHelper.getModel().U }, 
+			{ "V", modeHelper.getModel().V }
 		};
 		Utility::saveString(jResolvents.dump(4), BASE_FOLDER + output_folder + "resolvents.json.gz");
 	}
 	else {
 		std::cout << "Resolvent returned an empty vector." << std::endl;
-	}*/
+	}
 }
