6d7e7658ed2ed1411ef3fbab884285536442b086
Joshua <joshua.althueser@gmail.com>
2024-10-07 08:19:42 +0200

    cas_E_cadelake

--- cpp/ContinuumSystem/cmake/ClusterCompilerFlags.cmake
+++ cpp/ContinuumSystem/cmake/ClusterCompilerFlags.cmake
@@ -6,7 +6,7 @@ function(SET_COMPILER_FLAGS TARGET)
             message(FATAL_ERROR "GCC version ${CMAKE_CXX_COMPILER_VERSION} is not supported. GCC 12.1 or newer is required for C++20 support.")
         endif()
 
-        target_compile_options(${TARGET} PRIVATE -Wall -Wno-sign-compare -fopenmp -march=casecadelake -O3)
+        target_compile_options(${TARGET} PRIVATE -Wall -Wno-sign-compare -fopenmp -march=cascadelake -O3)
     else()
         message(FATAL_ERROR "Unsupported compiler ${CMAKE_CXX_COMPILER_ID}. Only GCC is supported.")
     endif()
