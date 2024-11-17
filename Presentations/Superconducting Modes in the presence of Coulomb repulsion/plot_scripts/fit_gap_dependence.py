babdd5f540e0bd8b46314212ad512d2f3a70e719
Joshua <joshua.althueser@gmail.com>
2024-11-17 11:05:20 +0100

    paper stuff

--- Presentations/Superconducting Modes in the presence of Coulomb repulsion/plot_scripts/fit_gap_dependence.py
+++ Presentations/Superconducting Modes in the presence of Coulomb repulsion/plot_scripts/fit_gap_dependence.py
@@ -7,7 +7,8 @@ from get_data import *
 from scipy.optimize import curve_fit
 import os
 from uncertainties import ufloat
-import uncertainties.umath as unp
+import uncertainties.unumpy as unp
+from uncertainties import correlated_values
 
 k_F = 4.25
 screening_factor = 0.4107320221286488672 * np.sqrt(k_F)
@@ -51,16 +52,24 @@ dfs = [approx, nc, ls, ss]
 
 fig, ax = plt.subplots()
 
-for idx, (plot_data, label, screening) in enumerate(zip(dfs, ["Approx. g(k,k')$", "Full $g(k,k')$", r"$\lambda=1$", r"$\lambda=10^{-4}$"], [None, None, 1, 1e-4])):
+for idx, (plot_data, label, screening) in enumerate(zip(dfs, ["Approx. $g(k,k')$", "Full $g(k,k')$", r"$\lambda=1$", r"$\lambda=10^{-4}$"], [None, None, 1, 1e-4])):
     g_lin = np.linspace(plot_data["g"].min() - 0.015, plot_data["g"].max() + 0.02, 200)
     E_F = plot_data["E_F"].iloc[0]
     y_data = np.log(plot_data["Delta_max"] / (2. * plot_data["omega_D"].iloc[0]))
     ax.plot(plot_data["g"], y_data, ls="", marker=f"{idx+1}", ms=14, markeredgewidth=2, color=f"C{idx}", label=label)
+    
     popt, pcov = curve_fit(fit_func, plot_data["g"], y_data, p0=(2.7, 1, (idx-1)**3 * 0.05))
     ax.plot(g_lin, fit_func(g_lin, *popt), color=f"C{idx}", ls="--")
-    
-    devi = np.sqrt(np.diag(pcov))
-    alpha = unp.exp( ufloat(popt[0], devi[0]) )
+    # Confidence interval (essentially it's a perfect fit)
+    #u_opt = correlated_values(popt, pcov)
+    #y_fit_unc = fit_func(g_lin, *u_opt)
+    #y_fit_nominal = unp.nominal_values(y_fit_unc)
+    #y_fit_std = unp.std_devs(y_fit_unc)
+    #
+    #plt.fill_between(g_lin, y_fit_nominal - 1.96 * y_fit_std, y_fit_nominal + 1.96 * y_fit_std,
+    #                color=f"C{idx}", alpha=0.3)#, label="95% CI"
+#    devi = np.sqrt(np.diag(pcov))
+#    alpha = unp.exp( ufloat(popt[0], devi[0]) )
 #    print(label, ":")
 #    print(f"AM prediction: 1.0000   ||   Fit: {alpha}")
 #    print(f"AM prediction: 1.0000   ||   Fit: {ufloat(popt[1], devi[1])}")
