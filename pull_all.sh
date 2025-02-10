c651bda0fce3aba8b03ec0e9bf947b54539d801d
majesticrock <joshua.althueser@googlemail.com>
2025-02-10 13:51:29 +0100

    setup for cluster

---
+++ pull_all.sh
@@ -0,0 +1,18 @@
+eval "$(ssh-agent -s)"
+ssh-add
+
+pull_repo() {
+    local repo_path=$1
+    cd "$repo_path" || exit
+    git pull
+    cd - || exit
+}
+
+pull_repo "PhdUtility"
+pull_repo "PresentationsPhd"
+pull_repo "phd_plot_scripts"
+pull_repo "cpp/ContinuumSystem"
+pull_repo "cpp/FermionCommute"
+pull_repo "cpp/FlowCommutators"
+pull_repo "cpp/Hubbard"
+pull_repo "."
