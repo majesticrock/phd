fccfc23193ce25ffe62e0b44f59aecfcdabf5d34
majesticrock <joshua.althueser@googlemail.com>
2026-03-25 16:26:39 +0100

    mode commute

--- cpp/ModeCommutators/sources/ModeCommutators.cpp
+++ cpp/ModeCommutators/sources/ModeCommutators.cpp
@@ -19,7 +19,7 @@ std::vector<Term> get_hamiltonian()
 			Operator('q', 1, false, Index::Sigma, true), Operator('q', 1, false, Index::Sigma, false)
 			})
         );
-    const Term H_Ph(-1, Coefficient::RealInversionSymmetric("g", MomentumList({ 'q', 'p' }), std::function<void(Coefficient&)>([](Coefficient& coeff){ coeff.momenta.sort(); })),
+    const Term H_Ph(1, Coefficient::RealInversionSymmetric("g", MomentumList({ 'q', 'p' }), std::function<void(Coefficient&)>([](Coefficient& coeff){ coeff.momenta.sort(); })),
 		SumContainer{ MomentumSum({ 'p', 'q' }) },
 		std::vector<Operator>({
 			c_k_dagger.with_momentum('q'), c_minus_k_dagger.with_momentum('q'),
@@ -31,9 +31,8 @@ std::vector<Term> get_hamiltonian()
 int main(int argc, char** argv) {
 	std::vector<Term> hamiltonian = get_hamiltonian();
 	
-	const std::vector<Term> phase_term = std::vector<Term>({
-					Term(1, std::vector<Operator>({ c_minus_k, c_k })),
-					Term(-1, std::vector<Operator>({ c_k_dagger, c_minus_k_dagger }))
+	const std::vector<Term> pc_term = std::vector<Term>({
+					Term(1, std::vector<Operator>({ c_k_dagger, c_minus_k_dagger }))
 			});
 	const std::vector<Term> higgs_term = std::vector<Term>({
 					Term(1, std::vector<Operator>({ c_minus_k, c_k })),
@@ -90,5 +89,17 @@ int main(int argc, char** argv) {
 		<< delta_commutator
 		<< "\\end{align*}" << std::endl;
 	
+	std::vector<Term> pc_commutator = commutator(hamiltonian, pc_term);
+	clean_up(pc_commutator);
+	std::ranges::sort(pc_commutator, [](const Term& l, const Term& r) {
+		if (l.operators.size() < r.operators.size()) return true;
+		if (l.operators.size() > r.operators.size()) return false;
+		if (!l.operators.front().is_daggered && r.operators.front().is_daggered) return true;
+		return false;
+		});
+	std::cout << "\\begin{align*}\n\t[H, c_{k \\uparrow}^\\dagger c_{-k \\downarrow}^\\dagger ] = "
+		<< pc_commutator
+		<< "\\end{align*}" << std::endl;
+
 	return 0;
 }
