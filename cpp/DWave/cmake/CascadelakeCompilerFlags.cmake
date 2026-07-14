9870370685300875353ae5ffeff38cba5d52d035
Joshua <joshua.althueser@gmail.com>
2026-07-14 10:57:11 +0200

    Clean-up cmake

--- cpp/DWave/cmake/CascadelakeCompilerFlags.cmake
+++ cpp/DWave/cmake/CascadelakeCompilerFlags.cmake
@@ -6,7 +6,7 @@ function(SET_COMPILER_FLAGS TARGET)
             message(FATAL_ERROR "GCC version ${CMAKE_CXX_COMPILER_VERSION} is not supported. GCC 12.1 or newer is required for C++20 support.")
         endif()
 
-        target_compile_options(${TARGET} PRIVATE -Wall -Wno-sign-compare -fopenmp -march=cascadelake -O3 -ffast-math)
+        target_compile_options(${TARGET} PRIVATE -Wall -Wno-sign-compare  -march=cascadelake -O3 -ffast-math)
         target_compile_definitions(${TARGET} PRIVATE NDEBUG MROCK_CL1_CASCADE)
         SET_MKL_FLAGS(${TARGET})
     else()
--- cpp/ModeCommutators/Makefile
+++ cpp/ModeCommutators/Makefile
@@ -13,7 +13,7 @@ output.tex: $(BUILD_DIR)/Makefile
 
 $(BUILD_DIR)/Makefile: CMakeLists.txt sources/*
 	@mkdir -p $(BUILD_DIR)
-	@cd $(BUILD_DIR) && cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ ..
+	@cd $(BUILD_DIR) && cmake -DCMAKE_CXX_COMPILER=g++ ..
 
 $(BUILD_DIR)/main.pdf: output.tex main.tex
 	latexmk $(TeXOptions) main.tex 1> $(BUILD_DIR)/log || cat $(BUILD_DIR)/log
